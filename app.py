34092b126f3b7179c3b90253f964e24031d89c0a
ArtsemDev <thommy.python.dev@gmail.com>
2023-03-30 13:24:09 +0300

    ///

--- app.py
+++ app.py
@@ -1,2 +1,45 @@
-print('hello world')
-print('hello python')
+# print('hello world')
+# print('hello python')
+
+
+# text = 'qwerty'
+# print(text[::-2])
+
+
+# text = 'hello'
+# a = len(text)
+# print(text[a])
+
+# text = 'hello'
+# print(text[:100])
+
+
+# name = 'Alex'
+# age = 34
+
+# text = 'Hello %s(a), your age %d(b)' % {'b': age, 'a': name}
+# text = 'Hello {a}, your age {b}'.format(b=name, a=age)
+# text = f'Hello {name}, your age {age * 2}'
+# print(text)
+
+
+# text = 'hello | world | python'
+# words = text.split(' | ')
+# print(words)
+# text2 = ' separator '.join(words)
+# print(text2)
+
+
+# text = 'Hello python world python great!'
+# result = text.replace('python', 'питон', 1)
+# print(result)
+
+
+text = 'hello'
+print(text.isalpha())
+print(text.isdigit())
+print(text.isascii())
+print(text.isalnum())
+print(text.isspace())
+print(text.isprintable())
+print(text.isidentifier())
